170c8443e3842dbaf23f95bfea961e39b71938fe
Kilian Valkhof <kilian@kilianvalkhof.com>
2024-01-23 14:12:41 +0100

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+module.exports = {
+  apps: [
+    {
+      name: `polypane-tunnel`,
+      script: "node --experimental-modules ./bin/server.js --landing=https://polypane.app --domain=polypane.link --port=3005 --max-sockets=10 --secure",
+      watch: false,
+    },
+  ],
+};
