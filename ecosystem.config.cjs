5be6b57dd48b5aaaf3f884ec408620ab41d5ee98
Kilian Valkhof <kilian@kilianvalkhof.com>
2024-01-24 08:34:37 +0100

    add ports to ecosystem

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
   apps: [
     {
       name: `polypane-tunnel`,
-      script: "node --experimental-modules ./bin/server.js --landing=https://polypane.app --domain=polypane.link --port=3005 --max-sockets=10 --secure",
+      script: "node --experimental-modules ./bin/server.js --landing=https://polypane.app --domain=polypane.link --port=3005 --max-sockets=10 --secure --range=50000:52000",
       watch: false,
     },
   ],
